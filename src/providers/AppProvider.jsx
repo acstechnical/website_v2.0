7e5ac83ca46c87bf7f486814a87ba820c2f1f452
Nguyen Thang <nguyenquocthang304@gmail.com>
2026-03-03 15:45:59 +0700

    update repository base paths and favicon

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 export const AppProvider = ({ children }) => {
+  const basename = import.meta.env.BASE_URL;
+
   return (
-    <BrowserRouter basename="/website-version_3.0/">
+    <BrowserRouter basename={basename}>
       {children}
     </BrowserRouter>
   );
